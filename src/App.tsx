d02fe960118497bcabdc39aadd4500c27cd4af79
ashibeko <ashibeko@gmail.com>
2024-01-09 23:33:02 +0300

    1. add router 2. ad boostrap 3. create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import { Home } from "./pages/Home";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Header } from './componenets/header/Header';
+import { About } from './pages/About';
+import { Home } from './pages/Home';
 
 function App() {
   return (
-    <Home />
+    <>
+      <Header/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+        </Routes>
+      </BrowserRouter>
+    <div># Footer</div>
+    </>
   );
 }
 
---
+++ src/componenets/header/Header.tsx
@@ -0,0 +1,23 @@
+export function Header() {
+    return (
+        <header className="navbar navbar-expand-lg">
+            <div className="container">
+                <div className="navbar-brand">
+                    <a href="/" className="d-inline-flex link-body-emphasis text-decoration-none">
+                        <img src="logo192.png" alt="" width={32}/>
+                    </a>
+                </div>
+
+                <ul className="navbar-nav">
+                    <li className="nav-item"><a href="/" className="nav-link px-2 link-secondary">Home</a></li>
+                    <li className="nav-item"><a href="/about" className="nav-link px-2">About</a></li>
+                </ul>
+
+                <div className="">
+                    <button type="button" className="btn btn-outline-primary me-2">Login</button>
+                    <button type="button" className="btn btn-primary">Sign-up</button>
+                </div>
+            </div>
+        </header>
+    );
+}
