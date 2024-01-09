0cebbc0b12a0953dc2e9dc8e6ccfb49ced75d85b
ashibeko <ashibeko@gmail.com>
2024-01-09 23:54:32 +0300

    some sections on home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,69 @@
-import { Col, Container, Row } from "@autrm/common/components/grid";
 
 export function Home() {
   return (
-    <Container>
-      <Row>
-        <Col lg={12}>
-          #Home
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <section className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-tertiary">
+        <div className="col-md-6 p-lg-5 mx-auto my-5">
+          <h1 className="display-3 fw-bold">Designed for engineers</h1>
+          <h3 className="fw-normal text-muted mb-3">Build anything you want with Aperture</h3>
+        </div>
+      </section>    
+
+      <section className="container">
+        <div className="row row-cols-1 row-cols-md-3 mb-3 text-center">
+          <div className="col">
+            <div className="card mb-4 rounded-3 shadow-sm">
+              <div className="card-header py-3">
+                <h4 className="my-0 fw-normal">Free</h4>
+              </div>
+              <div className="card-body">
+                <h1 className="card-title pricing-card-title">$0<small className="text-body-secondary fw-light">/mo</small></h1>
+                <ul className="list-unstyled mt-3 mb-4">
+                  <li>10 users included</li>
+                  <li>2 GB of storage</li>
+                  <li>Email support</li>
+                  <li>Help center access</li>
+                </ul>
+                <button type="button" className="w-100 btn btn-lg btn-outline-primary">Sign up for free</button>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card mb-4 rounded-3 shadow-sm">
+              <div className="card-header py-3">
+                <h4 className="my-0 fw-normal">Pro</h4>
+              </div>
+              <div className="card-body">
+                <h1 className="card-title pricing-card-title">$15<small className="text-body-secondary fw-light">/mo</small></h1>
+                <ul className="list-unstyled mt-3 mb-4">
+                  <li>20 users included</li>
+                  <li>10 GB of storage</li>
+                  <li>Priority email support</li>
+                  <li>Help center access</li>
+                </ul>
+                <button type="button" className="w-100 btn btn-lg btn-primary">Get started</button>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card mb-4 rounded-3 shadow-sm border-primary">
+              <div className="card-header py-3 text-bg-primary border-primary">
+                <h4 className="my-0 fw-normal">Enterprise</h4>
+              </div>
+              <div className="card-body">
+                <h1 className="card-title pricing-card-title">$29<small className="text-body-secondary fw-light">/mo</small></h1>
+                <ul className="list-unstyled mt-3 mb-4">
+                  <li>30 users included</li>
+                  <li>15 GB of storage</li>
+                  <li>Phone and email support</li>
+                  <li>Help center access</li>
+                </ul>
+                <button type="button" className="w-100 btn btn-lg btn-primary">Contact us</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
